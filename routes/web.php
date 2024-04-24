4fab7457b2bfdcde35c0738bc22e13f1d28b6342
MinhTuan083 <yuunohop3@gmail.com>
2024-04-25 01:53:46 +0700

    add new routes read

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,7 @@ use App\Http\Controllers\CustomAuthController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-    // $user = new User();
-    // $alluser = $user::all();
-    // dd($alluser);
-});
-Route::get('/htmll', function(){
-    return view('form');
-    // $html = '<h1>Hoc get lap trinh web</h1>';
-    // return $html;
-});
-Route::post('/htmll', function(){
-    return '<h1>Hoc post lap trinh web</h1>';
 
-}); 
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
@@ -30,12 +16,11 @@ Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout')
 Route::post('registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
 
 Route::get('list', [CustomAuthController::class, 'listUser'])->name('user.list');
-
 Route::get('create', [CustomAuthController::class, 'createUser'])->name('createUser');
 Route::post('create', [CustomAuthController::class, 'postUser'])->name('crud_user.postUser');
 Route::get('delete', [CustomAuthController::class, 'deleteUser'])->name('crud_user.deleteUser');
 Route::get('list', [CustomAuthController::class, 'listUser'])->name('list');
-
+Route::get('read-user/{id}', [CustomAuthController::class, 'readUser'])->name('read.user');
 Route::get('edit-user/{id}', [CustomAuthController::class, 'editUser'])->name('edit.user');
 Route::get('update-user/{id}', [CustomAuthController::class, 'updateUser'])->name('update.user');
 
