ba4d8b15a926efcd09e2e5f023c9b871dfdce08b
phi1235 <phipari12345@gmail.com>
2024-05-03 04:16:38 +0700

    update lab-7

--- resources/views/crud_user/read.blade.php
+++ resources/views/crud_user/read.blade.php
@@ -20,52 +20,7 @@
                         <button type="button"><a href="{{ route('edit.user', ['id' => $users->id]) }}">Edit</a> </button>
                         </div>
                     </div>
-                <div class="container">
-                    <div class="row">
-                        <h4>Profile (1-1)</h4>
-                        @if(isset($user))
-                        First name : {{$user->profile->first_name}} <br>
-                        Last name : {{$user->profile->last_name}} <br>
-                        @endif
-                    </div>
-
-                    <div class="row">
-                        <h4>Danh sách bài viết đã viết (1 - N)</h4>
-                        <table>
-                            <thead>
-                            <th>ID</th>
-                            <th>Post name</th>
-                            </thead>
-                            <tbody>
-
-                            @foreach($user->posts as $post)
-                                <tr>
-                                    <td>{{ $post->post_id }}</td>
-                                    <td>{{ $post->post_name }}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div class="row">
-                        <h4>Danh sách sở thích (N-N)</h4>
-                        <table>
-                            <thead>
-                            <th>ID</th>
-                            <th>Favorite</th>
-                            </thead>
-                            <tbody>
-                            @foreach($user->favorities as $favorite)
-                                <tr>
-                                    <td>{{ $favorite->favorite_id }}</td>
-                                    <td>{{ $favorite->favorite_name }}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-            </div>
+         
             </div>
         </div>
     </main>
