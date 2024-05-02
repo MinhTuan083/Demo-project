99b1da3b309768b82bab5af112ff52438a037b60
QuyNgo2004 <quyngocompurter2601@gmail.com>
2024-05-02 14:35:27 +0700

    sql

--- database/migrations/2022_5_2_0000_user_profile.php
+++ database/migrations/2022_5_2_0000_user_profile.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_profile', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone')->nullable();
-            
-            $table->string('image')->nullable(); 
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('sex');
+            $table->string('address');
+            $table->string('phone')->nullable(); 
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_profile');
     }
 };
